802ef682b4248e324f8c9a6131d046624efca273
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2022-01-13 08:09:28 +0700

    Refactor code

--- src/routes/user/index.route.js
+++
@@ -1,9 +0,0 @@
-
-const productRoute = require('./product.route');
-const userRoute = require('express').Router();
-const userController = require('./../../controllers/user/users.controller')
-
-userRoute.use('/products', productRoute);
-userRoute.get('/:username', userController.getUserList)
-userRoute.get('/', userController.getUser)
-module.exports = userRoute;
--- src/routes/user/treatmentHistory.route.js
+++
@@ -1,5 +0,0 @@
-const treatmentHistoryRoute = require('express').Router();
-const treatmentHistoryController = require('../../controllers/user/treatmenthistory.controller');
-
-treatmentHistoryRoute.get("/treatmentHistory", treatmentHistoryController.getTreatmentHistory);
-module.exports = treatmentHistoryRoute;
